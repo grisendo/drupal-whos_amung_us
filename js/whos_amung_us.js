2534fe99a8461cf92c8b3c7a6080c48ac26c5f9b
grisendo <grisendo@gmail.com>
2013-03-19 18:38:42 +0000

    Block functionallity

---
+++ js/whos_amung_us.js
@@ -0,0 +1,23 @@
+(function ($) {
+
+  Drupal.behaviors.whosAmungUsColorPicker = {
+    attach: function (context, settings) {
+      $('.wau-has-color-picker:not(.color-processed)').each(
+        function () {
+          var $this = $(this);
+          var $id = $this.attr('id') + '-color-placeholder';
+          $this.addClass('color-processed');
+          $this.parent().append('<div id="' + $id + '"></div>');
+          $this.myfarb = $.farbtastic('#' + $id);
+          $this.myfarb.linkTo(
+            function (color) {
+              $this.val(color.replace('#', '').toLowerCase());
+            }
+          );
+          $this.myfarb.setColor('#' + $this.val());
+        }
+      );
+    }
+  };
+
+})(jQuery);
